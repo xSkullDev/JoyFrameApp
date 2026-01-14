4b761f73275a91cde26207557b411d5d0aa0271e
xSkullDev <x.skull.dev@gmail.com>
2026-01-15 01:24:26 +0700

    update

--- script.js
+++ script.js
@@ -20,8 +20,6 @@ async function startCamera() {
     await video.play();
     if (video.readyState >= 1) setCanvasSize();
     else video.addEventListener('loadedmetadata', setCanvasSize, { once: true });
-    // Mirror preview for more natural selfie view
-    try{ video.classList.add('mirror'); overlay.classList.add('mirror'); }catch(e){}
     requestAnimationFrame(loop);
   } catch (err) {
     console.error('Camera error', err);
